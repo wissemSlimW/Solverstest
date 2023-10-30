d1943d269d18b394a67f88ee7ef835222c1646fb
wissemslim <wissemslim.w@gmail.com>
2023-10-30 14:58:44 +0100

    logout

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -3,6 +3,6 @@ import Link from "next/link";
 export const Header = () => {
   return <header className="bg-white h-20 w-full flex place-items-center place-content-center 
    text-center">
-    <Link href={'/api/auth/login'}><h1 className="text-[34px] text-[#4C4D4F]">AI Ignition</h1></Link>
+    <Link href={'/api/auth/logout'}><h1 className="text-[34px] text-[#4C4D4F]">AI Ignition</h1></Link>
   </header>;
 };
